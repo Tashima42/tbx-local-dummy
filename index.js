41d71e36ac3153bc4ebe43256506db6c07241f9f
Pedro Tashima <ptashima@toolboxtve.com>
2022-04-14 16:33:44 -0300

    feat: token and user information

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 var globalClient
+var queryParams
 
 main()
 
 function main() {
   loadClient()
+  loadQueryParams()
+  validateState()
+  loadAuthorizationCode()
+}
+
+function loadQueryParams() {
+  const params = new URLSearchParams(window.location.search)
+  queryParams = paramsToObject(params)
+
+  function paramsToObject(entries) {
+    const result = {}
+    for (const [key, value] of entries) {
+      result[key] = value
+    }
+    return result
+  }
 }
 
 function loadClient() {
@@ -70,6 +87,18 @@ function generateState() {
   return state
 }
 
+function validateState() {
+  const state = queryParams.state
+  const savedState = localStorage.getItem("state")
+  if (!state && !savedState) return
+  if (state != savedState) alert("invalid state")
+}
+
+function loadAuthorizationCode() {
+  const code = queryParams.code
+  if (code) document.querySelector("#code").innerHTML = code
+}
+
 function generateRandomString(length) {
   let result = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -79,22 +108,38 @@ function generateRandomString(length) {
   }
   return result;
 }
-function onClick() {
+function generateToken() {
 
-  fetch('/oauth/token', {
+  fetch(globalClient.endpoints.token, {
     method: 'POST',
-    body: `code=${code}&client_secret=${secret}&client_id=${id}&grant_type=authorization_code`, // this is how we send that data up
+    body: `code=${queryParams.code}&client_secret=${globalClient.secret}&client_id=${globalClient.id}&grant_type=authorization_code`,
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',  // This is REALLY important
     },
   })
     .then(res => res.json())
     .then(res => {
-      console.log('Credentials', res)
       token = res.access_token
       tokenType = res.token_type
       refreshToken = res.refresh_token
+      localStorage.setItem("token", token)
       document.getElementById('token').innerText = token
-      document.getElementById('refresh_token').innerText = refreshToken
+    })
+}
+
+function getUserInfo() {
+
+  fetch(globalClient.endpoints.userInfo, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem("token")}`
+    },
+  })
+    .then(res => res.json())
+    .then(res => {
+      subscriber_id = res.subscriber_id
+      country_code = res.country_code
+      document.getElementById('subscriber_id').innerText = subscriber_id
+      document.getElementById('country_code').innerText = country_code
     })
 }
